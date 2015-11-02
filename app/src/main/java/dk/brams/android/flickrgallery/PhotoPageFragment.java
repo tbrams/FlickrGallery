48ba66ed6d7e0eae78113fdad39c1baea6eb53f1
tbrams <torben@brams.dk>
2015-11-02 11:03:00 +0700

    Integrated WebView implemented

---
+++ app/src/main/java/dk/brams/android/flickrgallery/PhotoPageFragment.java
@@ -0,0 +1,57 @@
+package dk.brams.android.flickrgallery;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by tbrams on 02/11/15.
+ */
+public class PhotoPageFragment extends VisibleFragment {
+    private static final String ARG_URI = "photo_page_url";
+
+    private Uri mUri;
+    private WebView mWebView;
+
+    public static PhotoPageFragment newInstance(Uri uri) {
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_URI, uri);
+
+        PhotoPageFragment fragment = new PhotoPageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mUri = getArguments().getParcelable(ARG_URI);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
+        mWebView = (WebView) v.findViewById(R.id.fragment_photo_page_web_view);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+
+        mWebView.loadUrl(mUri.toString());
+
+        return v;
+    }
+
+
+}
